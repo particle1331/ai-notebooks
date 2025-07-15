63af1b2a848df847a941d75e75a9b8fb8abf0ab4
particle1331 <54845098+particle1331@users.noreply.github.com>
2025-07-15 21:47:30 +0800

    Formatting fix

--- topics/cpp/code/01_cin.cpp
+++ topics/cpp/code/01_cin.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     string resp;
     cout << "\nEver heard of rubber duck debugging?" << endl;
     cin  >> resp;
-    if ((resp[0] == 'Y') || (resp[0] == 'y')){
+    if ((resp[0] == 'Y') || (resp[0] == 'y')) {
         cout << "cool B)" << endl;
     }
     else {
