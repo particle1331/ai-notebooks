9aea5060fb285ada9ad2f60ebda2814b53a4c8c3
particle1331 <54845098+particle1331@users.noreply.github.com>
2025-08-26 04:06:29 +0800

    Keyword search as retrieval

--- src/notebooks/utils.py
+++ src/notebooks/utils.py
@@ -1,4 +1,6 @@
 import os
+import builtins
+import textwrap
 
 
 def hello():
@@ -12,3 +14,13 @@ def load_dotenv(verbose=False):
             os.environ[k] = v.strip().strip('"')
             if verbose:
                 print(f"Loaded env variable: {k}")
+
+
+def print(*args, wrap: bool=False, width: int=80, **kwargs):
+    new_args = []
+    for arg in args:
+        if wrap and isinstance(arg, str):
+            new_args.append(textwrap.fill(arg, width=width))
+        else:
+            new_args.append(arg)
+    builtins.print(*new_args, **kwargs)
