48dbe06fd5b4b4d33e214d547cb6c7944526021d
particle1331 <54845098+particle1331@users.noreply.github.com>
2025-11-06 00:32:28 +0800

    Flet basics start

---
+++ projects/flet-greeting/src/main.py
@@ -0,0 +1,52 @@
+import flet as ft
+import time
+from random import randint
+
+
+hello_world = [
+    "Hello, world!",
+    "¡Hola, mundo!",
+    "Bonjour, monde !",
+    "Hallo, Welt!",
+    "Ciao, mondo!",
+    "Olá, mundo!",
+    "こんにちは、世界！",
+    "안녕하세요, 세계!",
+    "你好，世界！",
+    "مرحباً، يا عالم!",
+]
+
+
+def main(page: ft.Page):
+    greeting = ft.Text(hello_world[0], size=60, data=6)
+    n = len(hello_world)
+    
+    def roll_greeting(e):
+        # Force update rolling animation
+        greeting.value = ""
+        page.update()
+        time.sleep(0.2)
+        
+        # Force update final result
+        greeting.data = hello_world[randint(0, n - 1)]
+        greeting.value = str(greeting.data)
+        page.update()
+
+
+    page.floating_action_button = ft.FloatingActionButton(
+        content=ft.Icon(ft.Icons.CASINO, size=60),
+        on_click=roll_greeting,
+        height=60, width=60
+    )
+
+    page.add(
+        ft.Container(
+            greeting,
+            alignment=ft.alignment.center,
+            expand=True
+        )
+    )
+
+
+if __name__ == "__main__":
+    ft.app(main)
